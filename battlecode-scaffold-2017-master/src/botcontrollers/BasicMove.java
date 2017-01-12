cac3cae988c578f87b2ca53e17b7f35911eb58fd
Brian <Brian@Brian-Stream>
2017-01-12 09:04:29 -0800

    make basic move work with functionality from example

--- battlecode-scaffold-2017-master/src/botcontrollers/BasicMove.java
+++ battlecode-scaffold-2017-master/src/botcontrollers/BasicMove.java
@@ -25,10 +25,38 @@ public class BasicMove {
 	 * @return success or failure
 	 */
 	public static boolean tryMove(Direction direction) throws GameActionException{
-		if(rc.canMove(direction)&&!rc.hasMoved()){
-			rc.move(direction);
-			return true;
-		}
-		return false;
+		if(rc.hasMoved()){return false;}
+		return tryMove(direction, 10,5);
 	}
+	
+	static boolean tryMove(Direction dir, float degreeOffset, int checksPerSide) throws GameActionException {
+		if(rc.hasMoved()){return false;}
+        // First, try intended direction
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            return true;
+        }
+
+        // Now try a bunch of similar angles
+        boolean moved = false;
+        int currentCheck = 1;
+
+        while(currentCheck<=checksPerSide) {
+            // Try the offset of the left side
+            if(rc.canMove(dir.rotateLeftDegrees(degreeOffset*currentCheck))) {
+                rc.move(dir.rotateLeftDegrees(degreeOffset*currentCheck));
+                return true;
+            }
+            // Try the offset on the right side
+            if(rc.canMove(dir.rotateRightDegrees(degreeOffset*currentCheck))) {
+                rc.move(dir.rotateRightDegrees(degreeOffset*currentCheck));
+                return true;
+            }
+            // No move performed, try slightly further
+            currentCheck++;
+        }
+
+        // A move never happened, so return false.
+        return false;
+    }
 }
